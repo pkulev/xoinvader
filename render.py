d94da9dde09fff6297a338dacdb76a8db1cbad90
pankshok <kulyov.pavel@gmail.com>
2014-07-28 00:19:23 +0000

    remove singleton's check, I don't need this... Fix render_all.

--- render.py
+++ render.py
@@ -1,23 +1,19 @@
-
-
 class Renderer(object):
-    __created = False
     def __init__(self):
-        if self.__class__.__created:
-            raise Exception("Renderer already exists!")
         self.__objects = []
-        self.__class__.__created = True
 
-    def __del__(self):
-        self.__class__.__created = False
 
     def add_object(self, obj):
         self.__objects.append(obj)
 
+
     def remove_object(self, obj):
         self.__objects.remove(obj)
 
+
     def render_all(self, screen):
         for obj in self.__objects:
-            for image, pos in obj.get_render_data():
-                screen.addch(pos.y, pos.x, image, obj.style)
+            glob_pos, data_gen = obj.get_render_data()
+            for el in data_gen:
+                pos, image, style = el
+                screen.addch(glob_pos.y + pos.y, glob_pos.x + pos.x, image)
