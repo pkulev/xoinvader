4584803694347446ed530f975eed9ff73068426f
pankshok <kulyov.pavel@gmail.com>
2015-03-09 14:08:31 +0300

    add docstrings, remove unused imports, move no-self-used methods to functions

--- xoi.py
+++ xoi.py
@@ -1,15 +1,19 @@
 #! /usr/bin/env python3
 
+"""Main XOInvader module, that is entry point to game.
+
+Prepare nvironment for starting game and start it."""
+
 import sys
 import time
 import curses
-from collections import namedtuple
 
 from gui import WeaponWidget, Bar
 from ship import GenericXEnemy, Playership
-from utils import Point, Event, Surface, Color, style, Layout
-from render import Renderer, Renderable
+from utils import Point, Color, style, Layout
+from render import Renderer
 from common import Settings
+from curses_utils import create_curses_window, deinit_curses
 
 
 KEY = "KEY"
@@ -24,24 +28,34 @@ K_ESCAPE = 27
 MILLISECONDS_PER_FRAME = 16
 
 
+
 class App(object):
-    
-    settings = Settings()
-    
+    """Main game class that is game entry point.
+
+    Create window, settings and pass them to appropriate objects when it's
+    really needed.
+    """
+
     def __init__(self):
+        self.settings = Settings()
         self.settings.layout = Layout().init_layout()
         self.settings.border = self.settings.layout.field["border"]
-        self.settings.field  = Point(x=self.settings.border.x, y=self.settings.border.y-1)
+        self.settings.field = Point(x=self.settings.border.x,
+                                    y=self.settings.border.y-1)
 
-        self.screen = self.create_window(x=self.settings.border.x, y=self.settings.border.y)
+        self.screen = create_curses_window(ncols=self.settings.border.x,
+                                           nlines=self.settings.border.y)
         style.init_styles(curses)
 
         self.settings.renderer = Renderer(self.settings.border)
 
-        self.playership = Playership(self.settings.layout.field["playership"], self.settings.field, self.settings)
+        self.playership = Playership(self.settings.layout.field["playership"],
+                                     self.settings.field, self.settings)
         self.settings.renderer.add_object(self.playership)
 
-        self.enemy = GenericXEnemy(Point(x=15, y=3), self.settings.field, self.settings)
+        self.enemy = GenericXEnemy(Point(x=15, y=3), self.settings.field,
+                                   self.settings)
+
         self.settings.renderer.add_object(self.enemy)
         #gui
 
@@ -57,8 +71,8 @@ class App(object):
         self.sbar.status_style["dmgd"] = style.gui["sh_mid"]
 
         self.wbar = Bar("", self.settings.layout.gui["wbar"],
-                            self.playership.get_full_wcinfo,
-                            update_all=True)
+                        self.playership.get_full_wcinfo,
+                        update_all=True)
 
         for s in ["good", "dmgd", "crit"]:
             self.wbar.status_style[s] = style.gui["dp_ok"]
@@ -67,52 +81,18 @@ class App(object):
                                   self.playership.get_weapon_info)
 
         self.gui = [self.hbar, self.sbar, self.wbar, self.winfo]
-        for e in self.gui: self.settings.renderer.add_object(e)
+        for e in self.gui:
+            self.settings.renderer.add_object(e)
 
 
-    def create_window(self, x, y, a=0, b=0):
-        curses.initscr()
-        curses.start_color()
-
-        #user interface
-        curses.init_pair(Color.ui_norm, curses.COLOR_WHITE, curses.COLOR_BLACK)
-        curses.init_pair(Color.ui_yellow, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-
-        #damage panel
-        curses.init_pair(Color.dp_blank, curses.COLOR_BLACK, curses.COLOR_BLACK)
-        curses.init_pair(Color.dp_ok, curses.COLOR_WHITE, curses.COLOR_GREEN)
-        curses.init_pair(Color.dp_middle, curses.COLOR_WHITE, curses.COLOR_YELLOW)
-        curses.init_pair(Color.dp_critical, curses.COLOR_WHITE, curses.COLOR_RED)
-        curses.init_pair(Color.sh_ok, curses.COLOR_WHITE, curses.COLOR_BLUE)
-        curses.init_pair(Color.sh_mid, curses.COLOR_WHITE, curses.COLOR_CYAN)
-
-        #weapons
-        curses.init_pair(Color.blaster, curses.COLOR_GREEN, curses.COLOR_BLACK)
-        curses.init_pair(Color.laser, curses.COLOR_BLACK, curses.COLOR_RED)
-        curses.init_pair(Color.um, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-
-        screen = curses.newwin(y, x, a, b)
-        screen.keypad(1)
-        screen.nodelay(1)
-        curses.noecho()
-        curses.cbreak()
-        curses.curs_set(0)
-        return screen
-
-
-    def deinit(self):
-        self.screen.nodelay(0)
-        self.screen.keypad(0)
-        curses.nocbreak()
-        curses.echo()
-        curses.curs_set(1)
-        curses.endwin()
 
 
     def events(self):
+        """Handle events and give command to playership."""
+
         c = self.screen.getch()
         if c == K_ESCAPE:
-            self.deinit()
+            deinit_curses(self.screen)
             sys.exit(1)
         elif c == K_A:
             self.playership.move_left()
@@ -129,21 +109,26 @@ class App(object):
 
 
     def update(self):
+        """Update all object's state."""
         self.playership.update()
         self.enemy.update()
-        for e in self.gui: e.update()
+        for el in self.gui:
+            el.update()
 
     def render(self):
+        """Render GUI and renderable objects to screen."""
         self.screen.erase()
         self.screen.border(0)
         self.screen.addstr(0, 2, "Score: {} ".format(0))
-        self.screen.addstr(0, self.settings.field.x // 2 - 4, "XOInvader", curses.A_BOLD)
+        self.screen.addstr(0, self.settings.field.x // 2 - 4,
+                           "XOInvader", curses.A_BOLD)
 
         self.settings.renderer.render_all(self.screen)
 
         self.screen.refresh()
 
     def loop(self):
+        """Main game loop."""
         while True:
             start_time = time.perf_counter()
 
@@ -156,7 +141,9 @@ class App(object):
             if delta <= MILLISECONDS_PER_FRAME:
                 time.sleep((MILLISECONDS_PER_FRAME - delta) / 1000.0)
             #else: log
+
 def main():
+    """Entry point. Create application class and go to main loop."""
     app = App()
     app.loop()
 
