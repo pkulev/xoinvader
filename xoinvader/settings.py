9f401479498b6694a6a6040a50de65518290a4cd
pankshok <kulyov.pavel@gmail.com>
2015-04-09 21:29:53 +0300

    [settings]: add wrap_nested for wrapping nested dicts

--- xoinvader/settings.py
+++ xoinvader/settings.py
@@ -5,8 +5,17 @@
 
 class Settings(dict):
     """Container for storing all game settings."""
-    def __init__(self, *args, **kwargs):
+    
+    def __init__(self, wrap_nested=False, *args, **kwargs):
         super(Settings, self).__init__(*args, **kwargs)
+        if wrap_nested:
+            self._wrap_nested()
+
+    def _wrap_nested(self):
+        for key, value in self.iteritems():
+            if type(value) == dict:
+                self[key] = Settings(True, value)
+
     def __getattr__(self, name):
         return self[name]
 
