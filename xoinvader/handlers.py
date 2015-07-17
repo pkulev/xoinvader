c1d40b3ef0a3e522822904429362ad7888bec276
pankshok <kulyov.pavel@gmail.com>
2015-07-17 13:12:02 +0300

    rm dublicated code.

--- xoinvader/handlers.py
+++ xoinvader/handlers.py
@@ -1,38 +1,6 @@
-from xoinvader.keys import *
-
-
 class Command(object):
     def execute(self, actor):
         raise NotImplementedError
-
-
-class MoveLeftCommand(Command):
-    def execute(self, actor):
-        actor.move_left()
-
-class MoveRightCommand(Command):
-    def execute(self, actor):
-        actor.move_right()
-
-class NextWeaponCommand(Command):
-    def execute(self, actor):
-        actor.next_weapon()
-
-class PrevWeaponCommand(Command):
-    def execute(self, actor):
-        actor.prev_weapon()
-
-class ToggleFireCommand(Command):
-    def execute(self, actor):
-        actor.toggle_fire()
-
-class TakeDamageCommand(Command):
-    def execute(self, actor):
-        actor.take_damage(5)
-
-class ToMainMenuCommand(Command):
-    def execute(self, actor):
-        actor.owner.state = "MainMenuState"
 
 
 class Handler(object):
@@ -52,41 +20,3 @@ class Handler(object):
 
     def handle(self):
         raise NotImplementedError
-
-
-class InGameInputHandler(Handler):
-    def __init__(self, owner):
-        super(InGameInputHandler, self).__init__(owner)
-
-        self._command_map = {
-            K_A : MoveLeftCommand(),
-            K_D : MoveRightCommand(),
-            K_E : NextWeaponCommand(),
-            K_Q : PrevWeaponCommand(),
-            K_R : TakeDamageCommand(),
-            K_SPACE : ToggleFireCommand(),
-            K_ESCAPE : ToMainMenuCommand()
-        }
-
-    def handle(self):
-        key = self._screen.getch()
-        cmd = self._command_map.get(key, None)
-        if cmd:
-            if isinstance(cmd, ToMainMenuCommand):
-                cmd.execute(self._owner)
-            else:
-                cmd.execute(self._actor)
-
-
-
-class InGameEventHandler(Handler):
-    def __init__(self, owner):
-        super(InGameEventHandler, self).__init__(owner)
-
-        self._input_handler = InGameInputHandler(owner)
-
-    def handle(self):
-        self._input_handler.handle()
-        # Some other event logic
-
-
