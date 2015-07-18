deee1d3c0c30270ee4bb3922dae7b5fb1e4c85af
pankshok <kulyov.pavel@gmail.com>
2015-07-18 19:19:48 +0300

    mv Application to game.py XOInvader, some cleanups.

--- xoinvader/application.py
+++ xoinvader/application.py
@@ -1,32 +1,11 @@
 import time
-import curses
 
-# ?!
-from xoinvader.gui import WeaponWidget, Bar
-from xoinvader.ship import GenericXEnemy, Playership
-from xoinvader.utils import Point
-from xoinvader.render import Renderer
-from xoinvader.common import Settings
-from xoinvader.settings import dotdict
-from xoinvader.curses_utils import create_curses_window, style
 
-class _Application(object):
+class Application(object):
     def __init__(self, startup_args={}):
-        self._update_settings_from_args(startup_args)
         self._state = None
         self._states = {}
-
-        # Ms per frame
-        self._mspf = 16
-
-    def _update_settings_from_args(self, args):
-        pass
-
-    def create_window(self):
-        pass
-
-    def exit(self):
-        pass
+        self._mspf = None # ms per frame
 
     @property
     def state(self):
@@ -37,7 +16,7 @@ class _Application(object):
         if name in self._states:
             self._state = self._states[name]
         else:
-            raise KeyError("No such state {0}.".format(name))
+            raise KeyError("No such state: '{0}'.".format(name))
 
     def register_state(self, state):
         """Add new state and initiate it with owner."""
@@ -60,54 +39,3 @@ class _Application(object):
                 time.sleep((self._mspf - delta) / 1000.0)
             else:
                 pass # Log FPS drawdowns.
-
-
-
-class Application(_Application):
-    def __init__(self, startup_args={}):
-        super(Application, self).__init__(startup_args)
-        self._update_settings_from_args(startup_args)
-        self.screen = create_curses_window(
-                ncols=Settings.layout.field.border.x,
-                nlines=Settings.layout.field.border.y)
-
-        # Ms per frame
-        self._mspf = 16
-
-        style.init_styles(curses)
-        Settings.renderer = Renderer(Settings.layout.field.border)
-
-        self.actor = Playership(Settings.layout.field.player,
-                Settings.layout.field.edge, Settings)
-
-        Settings.renderer.add_object(self.actor)
-
-        self.enemy = GenericXEnemy(Point(x=15, y=3), Settings.layout.field.edge,
-                Settings)
-
-        Settings.renderer.add_object(self.enemy)
-
-        # GUI style mapping
-        self.gui = dotdict(
-                hull=Bar(pos=Settings.layout.gui.bar.health, prefix="Hull: ",
-                  stylemap={
-                      lambda val: 70.0 <= val <= 100.0 : style.gui["dp_ok"],
-                      lambda val: 35.0 <= val < 70.0 : style.gui["dp_middle"],
-                      lambda val: 0.0 <= val < 35.0 : style.gui["dp_critical"]
-                  }),
-            shield=Bar(pos=Settings.layout.gui.bar.shield, prefix="Shield: ",
-                    stylemap={
-                        lambda val: 70.0 <= val <= 100.0 : style.gui["sh_ok"],
-                        lambda val: 35.0 <= val < 70.0 : style.gui["sh_mid"],
-                        lambda val: 0.0 <= val < 35.0 : style.gui["dp_critical"]
-                    }),
-            weapon=Bar(pos=Settings.layout.gui.bar.weapon,
-                    stylemap={
-                        lambda val: 0.0 <= val <= 100.0 : style.gui["dp_ok"]
-                    }),
-            weapon_info=WeaponWidget(Settings.layout.gui.info.weapon,
-                                  self.actor.get_weapon_info)
-        )
-
-        for gui_object in self.gui.values():
-            Settings.renderer.add_object(gui_object)
