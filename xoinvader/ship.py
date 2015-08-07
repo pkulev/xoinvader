80777836fb8f005460aabfe980c8de3d5a478572
pankshok <kulyov.pavel@gmail.com>
2015-08-08 02:58:54 +0300

    Move some methods to Ship superclass.

--- xoinvader/ship.py
+++ xoinvader/ship.py
@@ -25,6 +25,7 @@ class Ship(Renderable):
         self._fire    = False
         self._weapon  = None
         self._weapons = InfiniteList()
+        self._wbay = None
 
         self._max_hull   = None
         self._max_shield = None
@@ -44,6 +45,29 @@ class Ship(Renderable):
         self._shield     = int(config.shield)
         self._max_hull   = int(config.max_hull)
         self._max_shield = int(config.max_shield)
+
+    @property
+    def max_hull(self):
+        return self._max_hull
+
+    @property
+    def max_shield(self):
+        return self._max_shield
+
+    def getHullPercentage(self):
+        return self._hull * 100.0 / self._max_hull
+
+    def getShieldPercentage(self):
+        return self._shield * 100.0 / self._max_shield
+
+    def getWeaponPercentage(self):
+        return self._weapon.loadPercentage()
+
+    def get_render_data(self):
+        return [self._pos], self._image.get_image()
+
+    def get_renderable_objects(self):
+        return self._weapons
 
     def move_left(self):
         self._dx = -1
@@ -79,7 +103,33 @@ class Ship(Renderable):
 
 
         if self._fire:
-            self._weapon.make_shot(Point(x=self._pos.x + 1, y=self._pos.y))
+            try:
+                self._weapon.make_shot(Point(x=self._pos.x + self._wbay.x,
+                                             y=self._pos.y + self._wbay.y))
+            except ValueError as e:
+                self.next_weapon()
+
+        self.refresh_shield()
+
+    def take_damage(self, damage):
+        if self._shield < damage:
+            rest_damage = damage - self._shield
+            self._shield = 0
+            self._hull -= rest_damage
+        else:
+            self._shield -= damage
+        if self._hull < 0:
+            self._hull = 0
+
+    def refresh_shield(self, amount=None):
+        if self._shield == self._max_shield:
+            return
+
+        a = amount if amount else 1
+        if self._shield + a > self._max_shield:
+            self._shield = self._max_shield
+        else:
+            self._shield += a
 
     def get_render_data(self):
         return ([self._pos], self._image.get_image())
@@ -95,7 +145,7 @@ class GenericXEnemy(Ship):
         self.add_weapon(EBlaster())
         self._settings.renderer.add_object(self._weapon)
         self._fire = True
-
+        self._wbay = Point(x=self._image.width // 2, y=1)
 
 class Playership(Ship):
 
@@ -120,70 +170,7 @@ class Playership(Ship):
         Mixer.register(".".join([self._type, "engine"]),
                        Settings.path.sound.ship[self._type].engine)
 
-
-    def update(self):
-        if self._pos.x == self._border.x - self._image.width - 1 and self._dx > 0:
-            self._pos.x = 0
-        elif self._pos.x == 1 and self._dx < 0:
-            self._pos.x = self._border.x - self._image.width
-
-        self._pos.x += self._dx
-        self._dx = 0
-
-        if self._fire:
-            try:
-                self._weapon.make_shot(Point(x=self._pos.x + self._wbay.x,
-                                             y=self._pos.y + self._wbay.y))
-            except ValueError as e:
-                self.next_weapon()
-
-        self.refresh_shield()
-
-
     def get_weapon_info(self):
         return "Weapon: {w} | [{c}/{m}]".format(w=self._weapon.type,
                                                 c=self._weapon.ammo,
                                                 m=self._weapon.max_ammo)
-
-    @property
-    def max_hull(self):
-        return self._max_hull
-
-    @property
-    def max_shield(self):
-        return self._max_shield
-
-    def getHullPercentage(self):
-        return self._hull * 100.0 / self._max_hull
-
-    def getShieldPercentage(self):
-        return self._shield * 100.0 / self._max_shield
-
-    def getWeaponPercentage(self):
-        return self._weapon.loadPercentage()
-
-    def get_render_data(self):
-        return [self._pos], self._image.get_image()
-
-    def get_renderable_objects(self):
-        return self._weapons
-
-    def take_damage(self, damage):
-        if self._shield < damage:
-            rest_damage = damage - self._shield
-            self._shield = 0
-            self._hull -= rest_damage
-        else:
-            self._shield -= damage
-        if self._hull < 0:
-            self._hull = 0
-
-    def refresh_shield(self, amount=None):
-        if self._shield == self._max_shield:
-            return
-
-        a = amount if amount else 1
-        if self._shield + a > self._max_shield:
-            self._shield = self._max_shield
-        else:
-            self._shield += a
