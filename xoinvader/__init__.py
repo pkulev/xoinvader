03950a58d66fb734b4b831fd5b45e39f85a6baf5
pankshok <kulyov.pavel@gmail.com>
2015-09-14 20:55:25 +0300

    Fix building.

--- xoinvader/__init__.py
+++ xoinvader/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
     xoinvader
     ~~~~~~~~~
@@ -8,21 +7,3 @@
     :copyright: (c) 2015 by Pavel Kulyov.
     :license: GPL, see LICENSE for more details.
 """
-
-from xoinvader import application
-from xoinvader import collision
-from xoinvader import common
-from xoinvader import curses_utils
-from xoinvader import game
-from xoinvader import gui
-from xoinvader import handlers
-from xoinvader import ingame
-from xoinvader import keys
-from xoinvader import menu
-from xoinvader import render
-from xoinvader import settings
-from xoinvader import ship
-from xoinvader import sound
-from xoinvader import state
-from xoinvader import utils
-from xoinvader import weapon
