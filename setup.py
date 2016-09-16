4b474cb944b08b1414893e1f1ec4c11a1b20405a
Pavel Kulyov <kulyov.pavel@gmail.com>
2016-09-16 17:19:39 +0300

    deploy: add classifiers

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ from setuptools import setup, find_packages
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
-def description():
+def read(filename):
     """Read description."""
-    with open(os.path.join(BASEDIR, "README.md")) as fd:
+    with open(os.path.join(BASEDIR, filename)) as fd:
         return fd.read()
 
 
@@ -19,19 +19,36 @@ def description():
 setup(
     name="XOInvader",
     description="Curses/Pygame space game",
-    long_description=description(),
+    version="0.1",
     author="Pavel Kulyov",
     author_email="kulyov.pavel@gmail.com",
-    version="0.1",
+    long_description=read("README.md"),
     url="http://www.g-v.im/",
     packages=find_packages(),
-    package_dir={"xoinvader": "xoinvader"},
-    include_package_data=True,
     license="MIT",
+    include_package_data=True,
     platforms="Posix",
     install_requires=[],
+
     entry_points="""
     [console_scripts]
     xoigame = xoinvader.game:main
-    """
+    """,
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console :: Curses",
+        "Environment :: X11 Applications",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Games/Entertainment :: Arcade",
+        "Topic :: Software Development :: Libraries :: pygame",
+    ]
 )
