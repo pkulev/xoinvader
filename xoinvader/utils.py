c90b2c33fa751b60ea4a85516a0d2229411ba171
Pavel Kulyov <kulyov.pavel@gmail.com>
2016-10-19 19:40:00 +0300

    utils: remove __dict__ overhead
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

--- xoinvader/utils.py
+++ xoinvader/utils.py
@@ -69,10 +69,12 @@ class Point(object):
     :rtype: `xoinvader.utils.Point`
     """
 
+    __slots__ = ["x", "y", "z"]
+
     def __init__(self, x=0, y=0, z=0):
-        self._x = x
-        self._y = y
-        self._z = z
+        self.x = x
+        self.y = y
+        self.z = z
 
     def __repr__(self):
         return "Point(x={0}, y={1}, z={2})".format(self.x, self.y, self.z)
@@ -87,48 +89,6 @@ class Point(object):
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y and self.z == other.z
-
-    @property
-    def x(self):
-        """x coordinate.
-
-        :getter: yes
-        :setter: yes
-        :type: integer
-        """
-        return self._x
-
-    @x.setter
-    def x(self, val):
-        self._x = val
-
-    @property
-    def y(self):
-        """y coordinate.
-
-        :getter: yes
-        :setter: yes
-        :type: integer
-        """
-        return self._y
-
-    @y.setter
-    def y(self, val):
-        self._y = val
-
-    @property
-    def z(self):
-        """z coordinate.
-
-        :getter: yes
-        :setter: yes
-        :type: integer
-        """
-        return self._z
-
-    @z.setter
-    def z(self, val):
-        self._z = val
 
 
 class Surface(object):
@@ -158,6 +118,8 @@ class Surface(object):
     #     [" "," ","O"," "," "],           O
     #     ["<","=","H","=",">"],         <=H=>
     #     [" ","*"," ","*"," "] ]         * *
+
+    __slots__ = ["_image", "_width", "_height", "_style"]
 
     def __init__(self, image, style=None):
         self._image = image
