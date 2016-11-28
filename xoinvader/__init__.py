f5adeb11760e2bc096e9e22679cbb9d90c095d64
Pavel Kulyov <kulyov.pavel@gmail.com>
2016-11-28 17:32:06 +0300

    Use init for custom settings

--- xoinvader/__init__.py
+++ xoinvader/__init__.py
@@ -4,6 +4,28 @@
 
     XO Invader is an ASCII ncurses-based top-down space shooter.
 
-    :copyright: (c) 2015 by Pavel Kulyov.
+    :copyright: (c) 2016 by Pavel Kulyov.
     :license: GPL, see LICENSE for more details.
 """
+
+from .common import update_system_settings
+
+
+class XOInitializationError(Exception):
+    pass
+
+
+def init(settings=None):
+    """Do engine initialization first.
+
+    :param dict settings:
+    """
+
+    # TODO: config-management: update this stub
+    try:
+        if settings:
+            update_system_settings(settings)
+    except Exception as exc:
+        print(dir(exc))
+        raise XOInitializationError(exc)
+
---
+++ xoinvader/tests/test_init.py
@@ -0,0 +1,14 @@
+import pytest
+
+import xoinvader
+from xoinvader.common import Settings
+from xoinvader.settings import dotdict
+
+
+def test_init():
+    Settings.system.test_section = dotdict(key="value")
+    xoinvader.init({"test_section": dotdict(another_key="another-value")})
+    assert "another-value" == Settings.system.test_section.another_key
+
+    with pytest.raises(xoinvader.XOInitializationError):
+        xoinvader.init("bad-value")
