006c661d7f7a8728bda922947648f4ee55fd39c3
taptap <alexey.e.egorov@gmail.com>
2017-01-24 19:17:34 +0300

    add enemy_wave module and tests
    EnemyWave is a [potentianlly base] class to create waves of incoming
    enemies. Under the hood, it doesn't have any functionality that is
    particularly bound to enemy ships, it's just a restartable storage of
    callbacks that are called when their timeout is expired.
    
    issue #50

---
+++ xoinvader/tests/test_enemy_wave.py
@@ -0,0 +1,34 @@
+import pytest
+
+from xoinvader.enemy_wave import EnemyWave
+
+
+def test_wave():
+    e = EnemyWave()
+
+    class MockObject(object):
+        def __init__(self):
+            self.value = 0
+
+        def add(self):
+            self.value += 10
+
+    a = MockObject()
+    b = MockObject()
+
+    e.add_event(10, a.add)
+    e.add_event(20, b.add)
+
+    e.speed = 10
+    e.update()
+    assert a.value == 0
+    assert b.value == 0
+    e.start()
+    e.update()
+    assert a.value == 10
+    assert b.value == 0
+    e.update()
+    assert a.value == 10
+    assert b.value == 10
+
+    assert not e.running
