65cc1748a2c2d13ad9013f18621d3847cb6ab3e1
Pavel Kulyov <kulyov.pavel@gmail.com>
2017-12-18 00:40:53 +0300

    tests: provide app and state mocking
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

---
+++ xoinvader/tests/conftest.py
@@ -0,0 +1,48 @@
+"""Pytest configuration."""
+
+import pytest
+
+from xoinvader import application
+from xoinvader import state
+
+
+@pytest.fixture
+def mock_application(request):
+
+    def inner():
+        return MockedApplication()
+
+    request.addfinalizer(MockedApplication._finalize)
+    return inner
+
+
+@pytest.fixture
+def mock_state(request):
+
+    def inner(mock_app=False):
+        if mock_app:
+            MockedApplication()
+        application.get_current().register_state(MockedState)
+        return application.get_current().state
+
+    def stop():
+        application.get_current().deregister_state(MockedState.__name__)
+        if not application.get_current().states:
+            MockedApplication._finalize()
+
+    request.addfinalizer(stop)
+
+    return inner
+
+
+class MockedApplication(application.Application):
+
+    @staticmethod
+    def _finalize():
+        app = application.get_current()
+        app.stop()
+        application._CURRENT_APPLICATION = None
+
+
+class MockedState(state.State):
+    pass
