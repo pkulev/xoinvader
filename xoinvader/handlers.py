63ce4225fa0e941c112d65c7b4b6e3a7d5dad2df
Pavel Kulyov <kulyov.pavel@gmail.com>
2017-12-27 17:11:59 +0300

    handlers: add EventHandler for handling input and events
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

--- xoinvader/handlers.py
+++ xoinvader/handlers.py
@@ -1,29 +1,17 @@
 """Base handler class."""
 
+from abc import ABCMeta, abstractmethod
 
-# pylint: disable=too-few-public-methods
-class Command(object):
-    """Base command class."""
-
-    def execute(self, actor):
-        """Execute command with actor.
-
-        :param actor: actor
-        :type: actor: object
-
-        .. note:: Make actor base class.
-        """
-        raise NotImplementedError
+import six
 
 
+@six.add_metaclass(ABCMeta)
 class Handler(object):
-    """
-    Base game handler.
+    """Base game handler.
 
     Provides accessing to main State's fields and handle function stub.
 
-    :param owner: handler's owner
-    :type owner: :class:`xoinvader.state.State`
+    :param :class:`xoinvader.state.State` owner: handler's owner
     """
 
     def __init__(self, owner):
@@ -31,6 +19,83 @@ class Handler(object):
         self._screen = owner.screen
         self._actor = owner.actor
 
+    @property
+    def actor(self):
+        """Controllable object.
+
+        :getter: yes
+        :setter: no
+        :type: object
+        """
+
+        return self._actor
+
+    @property
+    def owner(self):
+        """Handler's owner.
+
+        :getter: yes
+        :setter: no
+        :type: :class:`xoinvader.state.State`
+        """
+
+        return self._owner
+
+    @property
+    def screen(self):
+        """Screen.
+
+        :getter: yes
+        :setter: no
+        :type: object
+        """
+
+        return self._screen
+
+    @abstractmethod
     def handle(self):
         """Handle event."""
-        raise NotImplementedError
+
+        pass  # pragma: no cover
+
+
+# TODO: event-handling: now this is prototype that must be reworked in future
+# TODO: remove owner link if it's not needed anymore
+class EventHandler(Handler):
+    """Base game event handler.
+
+    Handles events using the event queue.
+    Provides command mapping manipulations via the default handle() method.
+
+    :param :class:`xoinvader.state.State` owner: handler's owner
+    :param dict command_map: key->command mapping
+    """
+
+    def __init__(self, owner, command_map=None):
+        super(EventHandler, self).__init__(owner)
+
+        self._command_map = command_map or {}
+
+    # TODO: event-handling: I think we need some global event bus in whole
+    #       application.
+    def event_queue(self):
+        """Event queue stub."""
+
+        return [("KEY_PRESS", self.get_input())]
+
+    # TODO: event-handling: abstract getting input from curses
+    def get_input(self):
+        """Get input from keyboard."""
+
+        return self._screen.getch()
+
+    def handle(self):
+        """Handle input event."""
+
+        for event in self.event_queue():
+            if event[0] == "KEY_PRESS":
+                command = self._command_map.get(event[1])
+                if callable(command):
+                    command()
+            else:
+                raise ValueError("Unknown event type: {0}".format(event[0]))
--- xoinvader/tests/test_handlers.py
+++ xoinvader/tests/test_handlers.py
@@ -2,23 +2,61 @@
 
 import pytest
 
-from xoinvader.handlers import Command
-from xoinvader.handlers import Handler
+from xoinvader import handlers
 
 
-# pylint: disable=missing-docstring,too-few-public-methods
-class OwnerMock:
-    screen = "screen"
+class ScreenMock(object):
+    """Screen mock."""
+
+    key = "test-key"
+
+    def getch(self):
+        """Mocked method to emit pressed key."""
+
+        return self.key
+
+    def mocked_event_queue(self):  # pylint: disable=no-self-use
+        """Mocked method to emit wrong event."""
+
+        return [("bad-event", "test")]
+
+
+class OwnerMock:  # pylint: disable=too-few-public-methods
+    """Owner mock to provide access to screen and actor for handler."""
+
+    screen = ScreenMock()
     actor = "actor"
 
 
-def test_command():
-    command = Command()
-    with pytest.raises(NotImplementedError):
-        command.execute("actor")
+def test_event_handler():
+    """Test xoinvader.handlers.EventHandler."""
+
+    events = handlers.EventHandler(OwnerMock())
+
+    assert isinstance(events.owner, OwnerMock)
+    assert isinstance(events.screen, ScreenMock)
+    assert events.actor == OwnerMock.actor
+
+    assert events.get_input() == ScreenMock.key
+    assert events.event_queue() == [("KEY_PRESS", ScreenMock.key)]
+
+    assert not events.handle()
+
+    fired = []
+    events = handlers.EventHandler(OwnerMock(), {
+        ScreenMock.key: lambda: fired.append(ScreenMock.key)
+    })
+
+    events.handle()
+    assert fired == [ScreenMock.key]
 
 
-def test_handler():
-    handler = Handler(OwnerMock())
-    with pytest.raises(NotImplementedError):
-        handler.handle()
+def test_event_handler_negative(monkeypatch):
+    """Negatively test xoinvader.handlers.EventHandler."""
+
+    monkeypatch.setattr(handlers.EventHandler, "event_queue",
+                        ScreenMock.mocked_event_queue)
+
+    events = handlers.EventHandler(OwnerMock())
+    with pytest.raises(ValueError):
+        events.handle()
