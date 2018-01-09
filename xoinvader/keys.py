3e1fd719d3a73c1ca66828e4d7f1cb6b4088d528
Pavel Kulyov <kulyov.pavel@gmail.com>
2018-01-09 14:01:52 +0300

    keys: provide convenient class-container for keys
    There's no need to import bunch of keys anymore, just use container.
    
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

--- xoinvader/keys.py
+++ xoinvader/keys.py
@@ -1,5 +1,9 @@
 """Keys' integer representation."""
 
+import six
+
+from xoinvader.utils import Singleton
+
 
 K_A = ord("a")
 K_D = ord("d")
@@ -13,3 +17,25 @@ K_W = ord("w")
 K_SPACE = ord(" ")
 K_ESCAPE = 27
 K_ENTER = 343
+
+
+@six.add_metaclass(Singleton)
+class _KEY(object):
+    """Convenient key container with dot access.
+
+    K_KEYNAME can be accessed via KEY.KEYNAME.
+    .. Attention:: this class relies to K_{KEYNAME} scheme.
+                   No more than one underscore allowed.
+    """
+
+    KEY_PREFIX = "K_"
+    """Prefix for all keys."""
+
+    def __init__(self):
+        for key, scancode in globals().items():
+            if key.startswith(self.KEY_PREFIX):
+                self.__dict__[key.split("_")[1]] = scancode
+
+
+KEY = _KEY()
+"""Key container object."""
