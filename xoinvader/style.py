3d05b45feee9a16575986c10eecb1ec021502242
Pavel Kulyov <kulyov.pavel@gmail.com>
2020-05-18 13:05:20 +0300

    style: remove curses attr construction
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

--- xoinvader/style.py
+++ xoinvader/style.py
@@ -1,7 +1,5 @@
 """XOInvader styles."""
 
-import curses
-
 from eaf.meta import Singleton
 
 
@@ -17,16 +15,14 @@ class Style(metaclass=Singleton):
     def init_styles(self, palette):
         """Initialize styles."""
 
-        cpair = curses.color_pair
-
-        self.gui["normal"] = cpair(palette.ui_norm) | curses.A_BOLD
-        self.gui["yellow"] = cpair(palette.ui_yellow) | curses.A_BOLD
-        self.gui["dp_blank"] = cpair(palette.dp_blank) | curses.A_BOLD
-        self.gui["dp_ok"] = cpair(palette.dp_ok) | curses.A_BOLD
-        self.gui["dp_middle"] = cpair(palette.dp_middle) | curses.A_BOLD
-        self.gui["dp_critical"] = cpair(palette.dp_critical) | curses.A_BOLD
-        self.gui["sh_ok"] = cpair(palette.sh_ok) | curses.A_BOLD
-        self.gui["sh_mid"] = cpair(palette.sh_mid) | curses.A_BOLD
+        self.gui["normal"] = palette.ui_norm
+        self.gui["yellow"] = palette.ui_yellow
+        self.gui["dp_blank"] = palette.dp_blank
+        self.gui["dp_ok"] = palette.dp_ok
+        self.gui["dp_middle"] = palette.dp_middle
+        self.gui["dp_critical"] = palette.dp_critical
+        self.gui["sh_ok"] = palette.sh_ok
+        self.gui["sh_mid"] = palette.sh_mid
 
     def __getattr__(self, name):
         return self._style[name]
